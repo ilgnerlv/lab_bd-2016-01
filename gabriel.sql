3ec82eef6cefec85c23ce36ec9c6775e6461e8e0
MartinsGabrielC <martins.gabriel.c@gmail.com>
2016-05-31 17:01:11 -0300

    Alterado Script Gabriel

--- gabriel.sql
+++ gabriel.sql
@@ -3,76 +3,76 @@ DROP TABLE Turma;
 DROP TABLE Sala;
 
 CREATE TABLE PertenceDD(
-	nome_departamento character varying(100),
-	codigo_disciplina CHAR(10),
-	CONSTRAINT pertencedd_nome_departamento foreign key (nome_departamento) references Departamento(dep_nome),
+	sigla_departamento character varying(100),
+	codigo_disciplina character varying(10),
+	CONSTRAINT pertencedd_sigla_departamento foreign key (sigla_departamento) references Departamento(dep_sigla),
 	CONSTRAINT pertencedd_codigo_disciplina foreign key (codigo_disciplina) references Disciplina(Codigo),
-	CONSTRAINT pertencedd_pk PRIMARY KEY (nome_departamento,codigo_disciplina_disciplina)
+	CONSTRAINT pertencedd_pk PRIMARY KEY (sigla_departamento,codigo_disciplina_disciplina)
  );
  
  CREATE TABLE Turma(
- 	id_turma CHAR(1),
- 	vagas int,
- 	codigo_disciplina CHAR(10),
- 	codigo_docente CHAR(10),
+ 	id_turma character varying(1),
+ 	vagas integer,
+ 	codigo_disciplina character varying(10),
+ 	codigo_docente integer,
  	CONSTRAINT turma_codigo_disciplina foreign key (codigo_disciplina) references Disciplina(Codigo),
  	CONSTRAINT turma_codigo_docente foreign key (codigo_docente) references Docente(Codigo),
  	CONSTRAINT turma_pk primary key (codigo_disciplina, id_turma)
  );
 
  CREATE TABLE Sala(
- 	id_turma CHAR(1),
- 	codigo_disciplina CHAR(10),
- 	sala char(20),
+ 	id_turma character varying(1),
+ 	codigo_disciplina character varying(10),
+ 	sala character varying(20),
  	CONSTRAINT sala_turma_fk foreign key (id_turma, codigo_disciplina) references Turma(id_turma, codigo_disciplina),
  	CONSTRAINT sala_pk PRIMARY KEY (id_turma, codigo_disciplina, sala)
  );
 
 INSERT INTO PertenceDD VALUES(
-	,--nome_departamento
-	'08806'
+	'DC',--sigla_departamnto
+	'08806'--cod_disciplina
 );
 
 INSERT INTO PertenceDD VALUES(
-	,--nome_departamento
-	'562312'
+	'DC',--sigla_departamnto
+	'562312'--cod_disciplina
 );
 
 INSERT INTO Turma VALUES(
-	'A',
-	30,
-	,--cod_disciplina
+	'A',--id_turma
+	30,--vagas
+	'08806',--cod_disciplina
 	--cod_docente
 );
 
 INSERT INTO Turma VALUES(
-	'B',
-	25,
-	,--cod_disciplina
+	'B',--id_turma
+	25,--vagas
+	'08806',--cod_disciplina
 	--cod_docente
 );
 
 INSERT INTO Turma VALUES(
-	'A',
-	60,
-	,--cod_disciplina
+	'A',--id_turma
+	60,--vagas
+	'562312',--cod_disciplina
 	--cod_docente
 );
 
 INSERT INTO Sala VALUES(
-	'A',
-	,--cod_disciplina
-	'AT9-129'
+	'A',--id_turma
+	'08806',--cod_disciplina
+	'AT9-129'--sala
 );
 
 INSERT INTO Sala VALUES(
-	'B',
-	,--cod_disciplina
-	'AT5-100'
+	'B',--id_turma
+	'08806',--cod_disciplina
+	'AT5-100'--sala
 );
 
 INSERT INTO Sala VALUES(
-	'A',
-	,--cod_disciplina
-	'AT9-131'
+	'A',--id_turma
+	'562312',--cod_disciplina
+	'AT9-131'--sala
 );
