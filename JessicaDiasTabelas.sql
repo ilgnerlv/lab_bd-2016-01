e25d6fb02376c20235660a7dfe127ffc35e97688
Jéssica Caroline <jc.jooy@gmail.com>
2016-05-31 14:46:40 -0300

    Update JessicaDiasTabelas.sql
    Pequenas correções (erros encontrados na hora de inserir dados na tabela)

--- JessicaDiasTabelas.sql
+++ JessicaDiasTabelas.sql
@@ -4,16 +4,16 @@ CREATE TABLE public.campus
   campus_website character varying(50),
   campus_sigla character varying(50) NOT NULL,
   CONSTRAINT campus_pk PRIMARY KEY (campus_sigla)
-)
+);
 
 CREATE TABLE public.centro
 (
   centro_nome character varying(100),
   centro_website character varying(50),
-  centro_geo point,
+  centro_geo character varying(50),
   centro_sigla character varying(50) NOT NULL,
   CONSTRAINT centro_pk PRIMARY KEY (centro_sigla)
-)
+);
 
 CREATE TABLE public.departamento
 (
@@ -25,7 +25,7 @@ CREATE TABLE public.departamento
   CONSTRAINT depcampus_fk FOREIGN KEY (campus_sigla)
 	REFERENCES public.campus (campus_sigla) MATCH SIMPLE
 	ON UPDATE NO ACTION ON DELETE NO ACTION
-)
+);
 
 -- Atividade complementar
 CREATE TABLE public.atcomp
@@ -33,15 +33,14 @@ CREATE TABLE public.atcomp
   at_creditos integer,
   at_nome character varying(100) NOT NULL,
   CONSTRAINT atcomp_pk PRIMARY KEY (at_nome)
-)
+);
 
 -- Relacionamento Realiza (Atividade_Complementar x Estudante)
 CREATE TABLE public.realizaace
 (
   RA_estudante integer,
   at_nome character varying(100),
-  
-
+ 
   CONSTRAINT realizaace_pk PRIMARY KEY (RA_estudante, at_nome),
   CONSTRAINT raestudante_fk FOREIGN KEY (RA_estudante)
       REFERENCES public.estudante (RA_estudante) MATCH SIMPLE
@@ -49,7 +48,7 @@ CREATE TABLE public.realizaace
   CONSTRAINT atnome_fk FOREIGN KEY (at_nome)
       REFERENCES public.atcomp (at_nome) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
-)
+);
 
 -- Atributo multivalorado Endereco
 CREATE TABLE public.endereco
@@ -72,7 +71,7 @@ CREATE TABLE public.endereco
   CONSTRAINT enddep_fk FOREIGN KEY (dep_sigla)
       REFERENCES public.departamento (dep_sigla) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
-)
+);
 
 -- Atributo multivalorado Telefone
 CREATE TABLE public.telefone
@@ -91,4 +90,4 @@ CREATE TABLE public.telefone
   CONSTRAINT teldep_fk FOREIGN KEY (dep_sigla)
       REFERENCES public.departamento (dep_sigla) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
-)
+);
